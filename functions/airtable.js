26c228d998ed311c9a35333c7f8671f03f745802
Joël Weber <joel.weber@live.nl>
2020-01-14 23:08:28 +0100

    Added roster import

---
+++ functions/airtable.js
@@ -0,0 +1,138 @@
+require("dotenv").config();
+const Papa = require("papaparse");
+const moment = require("moment");
+
+var Airtable = require("airtable");
+Airtable.configure({
+  endpointUrl: "https://api.airtable.com",
+  apiKey: process.env.API_KEY
+});
+
+const base = Airtable.base("app2IA0Bsp3pr9Syy");
+
+const statusCode = 200;
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "content-type": "application/json"
+};
+
+let status = "Succes"
+
+const getPersonalID = async id =>
+  new Promise((resolve, reject) => {
+    const personalIDs = [];
+    base("Medewerkers")
+      .select({
+        filterByFormula: '{Persoonsnummer} = "' + id + '"',
+        maxRecords: 1,
+        view: "Medewerkers"
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          // This function (`page`) will get called for each page of records.
+
+          records.forEach(record => {
+            personalIDs.push({
+              id: record.id,
+              personalID: record.get("Persoonsnummer")
+            });
+          });
+
+          // To fetch the next page of records, call `fetchNextPage`.
+          // If there are more records, `page` will get called again.
+          // If there are no more records, `done` will get called.
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            reject(err);
+          } else if (personalIDs.length) {
+            resolve(personalIDs[0]);
+          } else {
+            resolve(false);
+          }
+        }
+      );
+  });
+
+exports.handler = async function(event) {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 200, // <-- Important!
+      headers,
+      body: JSON.stringify({
+        status: "This was not a POST request!"
+      })
+    };
+  }
+
+  if (event.body === null || event.body === undefined) {
+    return {
+      statusCode: 200, // <-- Important!
+      headers,
+      body: JSON.stringify({
+        status: "Missing data!"
+      })
+    };
+  }
+
+  // Decode and parse incoming data
+  const body = JSON.parse(event.body);
+  const parsed = Papa.parse(decodeURIComponent(body.data), {
+    header: true
+  });
+
+  const data = parsed.data;
+
+  const importedIDs = [...new Set(data.map(x => x["Persnr."]).filter(Boolean))];
+  const Roster = [];
+
+  const IDsPromise = importedIDs.map(async importedID => {
+    const id = await getPersonalID(parseInt(importedID)).catch(err => {
+      console.error(err)
+      status = err
+    });
+    if (id) {
+      return (importedID = id);
+    } else {
+      return (importedID = null);
+    }
+  });
+
+  const uncleanIDs = await Promise.all(IDsPromise);
+  const IDs = uncleanIDs.filter(Boolean);
+
+  for (i in data) {
+    const shift = data[i];
+    const day = moment(shift.Dag, "DD-MM-YYYY")
+    const id = IDs.find(oldID => {
+      return oldID.personalID === parseInt(shift["Persnr."])
+    });
+    if (id && day && shift && shift.Tot != "00:00") {
+      Roster.push({
+        fields: {
+          Aanwezig: shift.Van + " - " + shift.Tot,
+          MDW: [id.id],
+          Datum: moment(day).format("YYYY-MM-DD")
+        }
+      });
+    }
+  }
+
+  while(Roster.length) {
+    await base("Dagplanning").create(Roster.splice(0,10), function(err, records) {
+      if (err) {
+        console.error(err);
+        status = err
+        return;
+      }
+    });
+  }
+
+  return {
+    statusCode: 200,
+    headers,
+    body: status
+  };
+};
--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,48 @@ import '../styles/main.scss';
 import "bootstrap";
 import "responsive-bootstrap-tabs";
 import "dragscroll"
+
+window.onload = function() {
+    var fileInput = document.getElementById("fileInput");
+    var container = document.getElementById("container");
+    var loader = document.getElementById("loader");
+    var fileDisplayArea = document.getElementById("fileDisplayArea");
+  
+    fileInput.addEventListener("change", function(e) {
+      var file = fileInput.files[0];
+      var http = new XMLHttpRequest();
+      var url = window.location.origin + "/.netlify/functions/airtable";
+      var textType = /text.*|application.*/;
+  
+      if (file.type.match(textType)) {
+        loader.style.display = "block";
+        container.style.display = "none";
+  
+        var reader = new FileReader();
+  
+        reader.onload = function(e) {
+          
+          var data = {
+            data: encodeURIComponent(reader.result)
+          };
+          console.log(data)
+          http.open("POST", url, true);
+          http.setRequestHeader("Content-type", "application/json");
+  
+          http.onreadystatechange = function() {
+            //Call a function when the state changes.
+            if (http.readyState == 4 && http.status == 200) {
+              loader.style.display = "none";
+              container.style.display = "block";
+              fileDisplayArea.innerText = http.responseText;
+            }
+          };
+          http.send(JSON.stringify(data));
+        };
+  
+        reader.readAsText(file);
+      } else {
+        fileDisplayArea.innerText = "Bestand niet ondersteund!";
+      }
+    });
+  };
