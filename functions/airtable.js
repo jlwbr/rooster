8cc99a2b3bd4aaa8946ec003cdaa1d5c51afb720
Joël Weber <joel@joelweber.nl>
2020-07-13 11:49:54 +0200

    Removed record deletion system

--- functions/airtable.js
+++ functions/airtable.js
@@ -51,97 +51,83 @@ exports.handler = async function (event, context, callback) {
 
     const body = JSON.parse(event.body)
     const data = parseCSV(decodeURIComponent(body.data))
+    try {
 
-    await base('Dagplanning').select({
-        view: "Rooster"
-    }).all().then(async records => {
-        const recordList = records.map(record => record.id)
-        while (recordList.length) {
-            await base('Dagplanning').destroy(recordList.splice(0, 10), function (err, deletedRecords) {
-                if (err) {
-                    status = err + "\n request ID: " + context.awsRequestId
-                    console.error(err);
-                    return;
-                }
-                console.log('Deleted', deletedRecords.length, 'records');
-            });
-        }
+        await base('Medewerkers').select({
+            view: "Medewerkers"
+        }).all().then(async records => {
+            let Roster = []
 
-        setTimeout(async () => {
-            await base('Medewerkers').select({
-                view: "Medewerkers"
-            }).all().then(async records => {
-                let Roster = []
+            for (i in data) {
+                const shift = data[i];
+                const day = moment(shift.Dag, "DD-MM-YYYY")
+                const id = records.find(record => record.get("Persoonsnummer") === parseInt(shift["Persnr."]))
 
-                for (i in data) {
-                    const shift = data[i];
-                    const day = moment(shift.Dag, "DD-MM-YYYY")
-                    const id = records.find(record => record.get("Persoonsnummer") === parseInt(shift["Persnr."]))
+                if (id && day && shift && shift.Tot != "00:00") {
+                    console.log("Adding Shift data: " + shift.Dag + " for employee: " + shift["Persnr."])
 
-                    if (id && day && shift && shift.Tot != "00:00") {
-                        console.log("Adding Shift data: " + shift.Dag + " for employee: " + shift["Persnr."])
-
-                        if (moment(shift.Van, "HH:mm").isBefore(moment("13:00", "HH:mm"))) {
-                            Roster.push({
-                                fields: {
-                                    Aanwezig: shift.Van + " - " + shift.Tot,
-                                    MDW: [id.id],
-                                    Datum: moment(day).format("YYYY-MM-DD"),
-                                    Dagdeel: "Ochtend",
-                                }
-                            });
-                            console.log("   Its an morning shift!")
-                        }
-                        if (moment(shift.Van, "HH:mm").isBetween(moment("13:00", "HH:mm"), moment("16:59", "HH:mm")) || (moment(shift.Tot, "HH:mm").isAfter(moment("13:00", "HH:mm")) && !moment(shift.Van, "HH:mm").isSameOrAfter(moment("17:00", "HH:mm")))) {
-                            Roster.push({
-                                fields: {
-                                    Aanwezig: shift.Van + " - " + shift.Tot,
-                                    MDW: [id.id],
-                                    Datum: moment(day).format("YYYY-MM-DD"),
-                                    Dagdeel: "Middag",
-                                }
-                            });
-                            console.log("   Its an afternoon shift!")
-                        }
-                        if (moment(shift.Van, "HH:mm").isAfter(moment("17:00", "HH:mm")) || moment(shift.Tot, "HH:mm").isAfter(moment("17:00", "HH:mm"))) {
-                            Roster.push({
-                                fields: {
-                                    Aanwezig: shift.Van + " - " + shift.Tot,
-                                    MDW: [id.id],
-                                    Datum: moment(day).format("YYYY-MM-DD"),
-                                    Dagdeel: "Avond",
-                                }
-                            });
-                            console.log("   Its an evening shift!")
-                        }
+                    if (moment(shift.Van, "HH:mm").isBefore(moment("13:00", "HH:mm"))) {
+                        Roster.push({
+                            fields: {
+                                Aanwezig: shift.Van + " - " + shift.Tot,
+                                MDW: [id.id],
+                                Datum: moment(day).format("YYYY-MM-DD"),
+                                Dagdeel: "Ochtend",
+                            }
+                        });
+                        console.log("   Its an morning shift!")
+                    }
+                    if (moment(shift.Van, "HH:mm").isBetween(moment("13:00", "HH:mm"), moment("16:59", "HH:mm")) || (moment(shift.Tot, "HH:mm").isAfter(moment("13:00", "HH:mm")) && !moment(shift.Van, "HH:mm").isSameOrAfter(moment("17:00", "HH:mm")))) {
+                        Roster.push({
+                            fields: {
+                                Aanwezig: shift.Van + " - " + shift.Tot,
+                                MDW: [id.id],
+                                Datum: moment(day).format("YYYY-MM-DD"),
+                                Dagdeel: "Middag",
+                            }
+                        });
+                        console.log("   Its an afternoon shift!")
+                    }
+                    if (moment(shift.Van, "HH:mm").isAfter(moment("17:00", "HH:mm")) || moment(shift.Tot, "HH:mm").isAfter(moment("17:00", "HH:mm"))) {
+                        Roster.push({
+                            fields: {
+                                Aanwezig: shift.Van + " - " + shift.Tot,
+                                MDW: [id.id],
+                                Datum: moment(day).format("YYYY-MM-DD"),
+                                Dagdeel: "Avond",
+                            }
+                        });
+                        console.log("   Its an evening shift!")
                     }
                 }
-                while (Roster.length) {
-                    console.log("Creating records")
-                    await base("Dagplanning").create(Roster.splice(0, 10), function (err, records) {
-                        if (err) {
-                            status = err + "\n request ID: " + context.awsRequestId
-                            console.error(err);
-                            return;
-                        }
-                    });
-                }
-            }).catch(err => {
-                if (err) {
-                    status = err + "\n request ID: " + context.awsRequestId
-                    console.error(err);
-                    return;
-                }
-            })
-        }, 1000)
-    }).catch(err => {
+            }
+            while (Roster.length) {
+                console.log("Creating records")
+                await base("Dagplanning").create(Roster.splice(0, 10), function (err, records) {
+                    if (err) {
+                        status = err + "\n request ID: " + context.awsRequestId
+                        console.error(err);
+                        return;
+                    }
+                });
+            }
+        }).catch(err => {
+            if (err) {
+                status = err + "\n request ID: " + context.awsRequestId
+                console.error(err);
+                return;
+            }
+        })
+        
+    }
+    catch (err) {
         if (err) {
             status = err + "\n request ID: " + context.awsRequestId
             console.error(err);
             return;
         }
-    })
 
+    }
     return {
         statusCode: 200,
         headers,
